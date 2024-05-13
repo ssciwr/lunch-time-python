944796df62fe35339d163c5a49ca3b54bd11e93e
Liam Keegan <liam@keegan.ch>
2024-05-13 15:17:20 +0200

    Add header to generated slides

---
+++ jupyter_nbconvert_config.py
@@ -0,0 +1,9 @@
+c = get_config()  # noqa
+c.ExecutePreprocessor.enabled = True
+c.ExecutePreprocessor.kernel_name = "python"
+c.NbConvertApp.export_format = "slides"
+c.SlidesExporter.extra_template_paths = ["."]
+c.SlidesExporter.file_extension = ".html"
+c.SlidesExporter.reveal_transition = "convex"
+c.SlidesExporter.reveal_scroll = True
+c.SlidesExporter.template_name = "jupyter_nbconvert_template"
